af24992874777434be08ba967dbc72e7b9de9d7f
Tom Manley <tom.manley@gmail.com>
2014-01-30 22:38:32 -0600

    Updated beer list

--- scripts/update_db.py
+++ scripts/update_db.py
@@ -10,7 +10,7 @@ with app.app_context():
                     abv="5.4%",
                     description="""Short Circuit Stout is a full-bodied and roasty stout with a lingering sweet aftertaste.
 It uses 5 different malts to create a variety of flavors that change through the course of taking a sip.""",
-                    image="http://placekitten.com/200/200"))
+                    image="/images/short-circuit-stout.jpg"))
     session.merge(Tap(id=2,
                     name="Flip Switch IPA",
                     style="American IPA",
@@ -18,4 +18,12 @@ It uses 5 different malts to create a variety of flavors that change through the
                     description="""Flip Switch IPA is a hoppy American pale ale with a slight citrus aroma. It features
 a variety of American hops, with just the right amount of bitterness to match the malty base.""",
                     image="http://placekitten.com/200/200"))
+    session.merge(Tap(id=3,
+                      name="Wheatstone Bridge",
+                      style="American style wheat",
+                      abv="6.3%",
+                      description="""Wheatstone Bridge is a light and refreshing wheat beer with distinct flavors of
+honey and chamomile tea. Made by adding Minnesota honey to an American malted wheat base, the final step involves
+steeping chamomile tea.""",
+                      image="http://placekitten.com/200/200"))
     session.commit()
